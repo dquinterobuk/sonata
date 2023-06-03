1979a68c4db081c9fe2685bcd5c35770a032bb5e
David Fernando Quintero <dquintero@buk.co>
2023-06-03 14:11:59 -0500

    paso 10

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,9 +13,30 @@ use Symfony\Component\Routing\Annotation\Route;
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(ConferenceRepository $conferenceRepository): Response
     {   
-        return $this->render('conference/index.html.twig');
+        $conferences = $conferenceRepository->findAll();
+        return $this->render('conference/index.html.twig', [
+            'conferences' => $conferences,
+        ]);
 
+    }
+
+    /**
+     * @Route("/conference/{id}", name="conference")
+     */
+    public function show(Request $request, CommentRepository $commentRepository, Conference $conference){
+
+        $offset = max(0, $request->query->getInt('offset', 0));
+
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+        return $this->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset +
+            CommentRepository::PAGINATOR_PER_PAGE),
+            ]);
     }
 }
