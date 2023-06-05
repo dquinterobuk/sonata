7c378731faeca713c8792440d67857b8206106b4
David Fernando Quintero <dquintero@buk.co>
2023-06-05 18:38:20 -0500

    paso 14a

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,6 +16,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/', name: 'homepage')]
     public function index(ConferenceRepository $conferenceRepository): Response
     {   
@@ -31,6 +41,20 @@ class ConferenceController extends AbstractController
      */
     public function show(Request $request, CommentRepository $commentRepository, Conference $conference){
 
+        $comment = new Comment();
+        
+        $form = $this->createForm(CommentFormType::class, $comment);
+        
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setConference($conference);
+
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
+        
         $offset = max(0, $request->query->getInt('offset', 0));
 
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
@@ -41,6 +65,7 @@ class ConferenceController extends AbstractController
             'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
             'next' => min(count($paginator), $offset +
             CommentRepository::PAGINATOR_PER_PAGE),
-            ]);
+            'comment_form' => $form->createView(),
+        ]);
     }
 }
